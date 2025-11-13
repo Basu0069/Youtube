a678146839000514cde7302d339462606c5ac9cf
Basu0069 <netflixbasu1@gmail.com>
2025-11-14 01:48:02 +0530

    updated

--- Node/07_/index.js
+++ Node/07_/index.js
@@ -5,6 +5,8 @@ const fs  = require('fs');
 
 
 // NOw you have to set the view engine too okay 
+// this is you shhowing the file handle part okie 
+
 app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -12,9 +14,37 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function(req,res){
     fs.readdir(`./files`,function(err,files){
-        res.render("index");
+        // jab folder read ho jaye uske baad hi response render ho 
+        res.render("index", {files: files});
     })
 })
+
+
+
+
+app.get('/file/:filename',function(req,res){
+fs.readFile(`./files/${req.params.filename}`,"utf-8", function(err,filedata){
+res.render('show', {filename: req.params.filename, filedata: filedata});
+    })
+})
+
+
+
+
+app.post("/create", function(req,res){
+     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details,function(err){
+        res.redirect("/");
+        // files create hote hi vapis is page pe aajayegyege okayie 
+        
+     });
+    })
+
+
+
+
+
+
+
 
 
 app.listen(3000);
